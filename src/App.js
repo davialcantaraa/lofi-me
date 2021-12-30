fb88d8e4b0c67994c9be98d5db36edec9a8713b3
Davi Alcântara <dxvialcantara@gmail.com>
2021-12-30 16:08:46 -0300

    initial project setup, created global styles, created Player component and it functionalities

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Player from './components/Player';
+import './styles/global.scss';
+
+function App() {
+	return (
+		<div className="App">
+			<Player />
+		</div>
+	);
+}
+
+export default App;
