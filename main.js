3b5db87b3745d0e1ec7000319df0359186cdd191
Davi Alcântara <dxvialcantara@gmail.com>
2022-01-07 13:13:49 -0300

    added window postion persistance

--- main.js
+++ main.js
@@ -1,32 +1,43 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
+const Store = require('electron-store');
 const path = require('path');
+
+const store = new Store();
 
 function createWindow() {
 	const win = new BrowserWindow({
 		width: 200,
 		height: 100,
 		frame: false,
-		resizable: true,
+		resizable: false,
 		alwaysOnTop: true,
-		transparent: true,
+		// transparent: true,
 		webPreferences: {
 			preload: path.join(__dirname, 'preload.js'),
 			nodeIntegration: true,
 			contextIsolation: false,
 		},
 	});
-	win.loadURL('http://localhost:3000');
+	win.setPosition(store.get('positionX'), store.get('positionY'));
+	win.on('move', (event) => {
+		store.set('positionX', win.getPosition()[0]);
+		store.set('positionY', win.getPosition()[1]);
+	});
 	ipcMain.on('closeMenu', (event, arg) => {
+		win.setResizable(true);
 		win.setSize(200, 100, true);
+		win.setResizable(false);
 	});
 	ipcMain.on('openMenu', (event, arg) => {
+		win.setResizable(true);
 		win.setSize(200, 305, true);
+		win.setResizable(false);
 	});
+	win.loadURL('http://localhost:3000');
 }
 
 app.whenReady().then(() => {
 	createWindow();
-
 	app.on('activate', () => {
 		if (BrowserWindow.getAllWindows().length === 0) {
 			createWindow();
