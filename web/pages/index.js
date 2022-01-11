c61953c11d1684d3ab43d85cfc59eb4ac47fef24
Davi Alcântara <dxvialcantara@gmail.com>
2022-01-10 22:24:34 -0300

    started next app

---
+++ web/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Lofi.me</title>
+			</Head>
+			<main>
+				<h1>Hello world</h1>
+			</main>
+		</div>
+	);
+}
