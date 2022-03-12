e65b6a30dcef678af692485e91ee64fd11144aa6
Davi Alcântara <dxvialcantara@gmail.com>
2022-03-12 09:47:44 -0300

    created delelopment environment

--- packages/app/src/services/api.js
+++ packages/app/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+	baseURL: 'http://localhost:5000',
 });
 
 export default api;
+
+// process.env.REACT_APP_API_URL ||
